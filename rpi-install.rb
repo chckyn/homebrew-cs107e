d4682b06892c73dad49f0c292b71e88fb062133d
Charles Celerier <chckyn@gmail.com>
2018-11-14 18:15:03 -0500

    rpi-install.rb: Fix rpi-install.py URL

--- rpi-install.rb
+++ rpi-install.rb
@@ -1,9 +1,9 @@
 class RpiInstall < Formula
   desc "Raspberry Pi uploading script for cs107e"
   homepage "https://cs107e.github.io/guides/mac_toolchain/"
-  url "https://cs107e.github.io/guides/mac_toolchain/rpi-install.py"
+  url "https://raw.githubusercontent.com/cs107e/cs107e.github.io/master/cs107e/bin/rpi-install.py"
   version "0.91-cs107e"
-  sha256 "d95eb9c26fe6b704292d83fbdbf91ce1d8197e810b615ffd5eaea5501d635260"
+  sha256 "45b19b7c3cc346a961d01dc2fa5511ce4989b3767d26314e26e8c14e6c7bdd16"
 
   def install
     bin.install "rpi-install.py"
